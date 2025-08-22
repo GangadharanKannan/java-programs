bb58d2600de8840a3fe4066faa9b280cbc3796c9
GangadharanKannan <ganga992004@gmail.com>
2025-08-22 20:48:57 +0530

    fourth-commit

---
+++ src/Strings.java
@@ -0,0 +1,12 @@
+public class Strings {
+    public static void main(String[] args) {
+        String name = "Gangadharan";
+        System.out.println(name.length());
+        System.out.println(name.toUpperCase());
+        System.out.println(name.toLowerCase());
+        System.out.println(name.trim());System.out.println(name.equals("Gangadharan"));
+        System.out.println(name == "gangadharan");
+        System.out.println(name.contains("Ganga"));
+        System.out.println(name.charAt(1));
+    }
+}
